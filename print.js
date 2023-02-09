5c5303765977893bf785db73fbe687b3d337cfc9
Petr Jurasek <petr.jurasek@tatum.io>
2023-02-09 08:52:18 +0100

    obj

--- print.js
+++ print.js
@@ -3,7 +3,17 @@ console.log("Hello world")
 const foo = process.env.FOO
 const bar = process.env.BAR
 
+const obj = {
+    foo : foo,
+    bar: bar,
+}
+
 console.log(foo)
 console.log(bar)
 
 console.log(`Super trooper env variable is: ${foo}, ${bar}`)
+
+console.log(obj)
+console.log(`Super trooper object is: ${JSON.stringify(obj)}`)
+
+
