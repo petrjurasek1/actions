148fba88a512f19527857eab3e3b8ed8a4b8d433
Petr Jurasek <petr.jurasek@tatum.io>
2023-02-09 08:45:00 +0100

    Actions log

--- print.js
+++ print.js
@@ -1,1 +1,9 @@
 console.log("Hello world")
+
+const foo = process.env.FOO
+const bar = process.env.BAR
+
+console.log(foo)
+console.log(bar)
+
+console.log(`Super trooper env variable is: ${foo}, ${bar}`)
